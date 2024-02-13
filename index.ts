d0b60904bedb47ff4dd93f81acd37132278454a8
marlonerler <marlon@erler.cc>
2024-02-13 19:15:40 +0100

    setup everything

--- index.ts
+++ index.ts
@@ -1,11 +1,23 @@
+import Crypto from "crypto";
 import Express from "express";
 import ExpressWs from "express-ws";
+import ExpressSession from "express-session";
 
 const app = Express();
+const sessionConfig = {
+  secret: Crypto.randomBytes(8).toString("hex"),
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false },
+};
+if (app.get("env") === "production") {
+  app.set("trust proxy", 1);
+  sessionConfig.cookie.secure = true; // serve secure cookies
+}
+app.use(ExpressSession(sessionConfig));
 ExpressWs(app);
 
 app.ws("/", (ws) => {
-
   ws.on("message", (data) => {
     console.log(data);
     ws.send(`You said: "${data}"`);
@@ -13,5 +25,15 @@ app.ws("/", (ws) => {
 });
 
 app.use("/tests", Express.static("Tests"));
+
+app.get("/", (req, res) => {
+  if (req.session.views) {
+    req.session.views += 1;
+    res.end(`Visit number ${req.session.views}`);
+  } else {
+    req.session.views = 1;
+    res.end("Welcome!");
+  }
+});
 
 app.listen(8000);
